b714e1b1a8820afdb7512517f4f881bc6e6cb73c
Krypton-ai <discorddeveloperren@gmail.com>
2024-06-22 17:26:11 +0800

    Update SourceLoadstring.lua

--- SourceLoadstring.lua
+++ SourceLoadstring.lua
@@ -1,2 +1,12 @@
 BootableURL = "https://github.com/Krypton-ai/Krypton/raw/main/a2.5/source.lua"
-loadstring(game:HttpGet(BootableURL))()
+local KryptonVersion = "a2.5"
+
+if _G.License == false then
+  error("Please accept our license")
+  warn("Krypton Version " .. KryptonVersion)
+  game:GetService'StarterGui':SetCore("DevConsoleVisible", true)
+  task.wait(1)
+  while true do end
+else
+  loadstring(game:HttpGet(BootableURL))()
+end
