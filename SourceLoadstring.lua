83c2e789f1445a84812739adbeaa870ad65205da
Krypton-ai <discorddeveloperren@gmail.com>
2024-06-24 10:03:41 +0800

    Update SourceLoadstring.lua

--- SourceLoadstring.lua
+++ SourceLoadstring.lua
@@ -1,6 +1,23 @@
-BootableURL = "https://github.com/Krypton-ai/Krypton/raw/main/a2.5/source.lua"
-local KryptonVersion = "a2.5"
+BootableURL = "https://github.com/Krypton-ai/Krypton/raw/main/a3.0/source.lua"
+local KryptonVersion = "a3.0"
 
+--[] Version Loader
+if _G.RewindKrypton == KryptonVersion then
+    warn("No need to rewind Krypton is already using the latest version")
+    game:GetService('StarterGui'):SetCore("DevConsoleVisible", true)
+elseif _G.RewindKrypton == "a2.5" then
+    loadstring(game:HttpGet("https://github.com/Krypton-ai/Krypton/raw/main/a2.5/source.lua"))()
+elseif _G.RewindKrypton == "a2.0" then
+    loadstring(game:HttpGet("https://github.com/Krypton-ai/Krypton/raw/main/a2.0/source.lua"))()
+else
+    warn("Invalid Krypton version")
+    game:GetService('StarterGui'):SetCore("DevConsoleVisible", true)
+end
+
+    
+
+
+--[] License
 if not _G.License then
     error("The license variable is missing!")
     warn("Add it back by adding _G.License = true")
