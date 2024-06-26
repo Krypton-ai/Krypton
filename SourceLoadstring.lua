ff354856cd7ca71df99ffe040048068147450c13
Krypton-ai <discorddeveloperren@gmail.com>
2024-06-26 21:01:02 +0800

    Update SourceLoadstring.lua

--- SourceLoadstring.lua
+++ SourceLoadstring.lua
@@ -1,40 +1,47 @@
--- Define constants for the latest bootable URL and the latest Krypton version
-LatestBootableURL = "https://raw.githubusercontent.com/Krypton-ai/Krypton/main/a3.3/Source.lua"
-LatestKryptonVersion = "a3.3"
+LatestVersion = "a3.4"
+LatestVersionURL = "https://raw.githubusercontent.com/Krypton-ai/Krypton/main/a3.4/Source.lua"
 
--- Function to rewind Krypton to a specified version
-function RewindKrypton()
-    if _G.RewindKrypton == "a3.3" then
-        warn("No need to rewind, Krypton is already using the latest version!")
-    elseif _G.RewindKrypton == "a2.0" then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Krypton-ai/Krypton/main/a2.0/Source.lua"))()
-    elseif _G.RewindKrypton == "a2.5" then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Krypton-ai/Krypton/main/a2.5/source.lua"))()
-    elseif _G.RewindKrypton == "a3.0" then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Krypton-ai/Krypton/main/a3.0/source.lua"))()
-    elseif _G.RewindKrypton == "a3.2" then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Krypton-ai/Krypton/main/a3.2/source.lua"))()
-    else
-        warn("Invalid Krypton Version!")
-        game:GetService('StarterGui'):SetCore("DevConsoleVisible", true)
-    end
-end
-
--- Check if the license is not accepted
-if not _G.License then
-    error("Please add back _G.License and accept our license!")
-end
-
--- Function to boot the latest version of Krypton
 function KryptonBoot()
-    loadstring(game:HttpGet(LatestBootableURL))()
+  if _G.KryptonVersion == LatestVersion then
+    warn("Using version :" .. _G.KryptonVersion)
+    loadstring(game:HttpGet(LatestVersionURL))()
+  elseif _G.KryptonVersion == "a3.3" then
+    warn("Using version :" .. _G.KryptonVersion)
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Krypton-ai/Krypton/main/a3.3/Source.lua'))()
+  elseif _G.KryptonVersion == "a3.2" then
+    warn("Using version :" .. _G.KryptonVersion)
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Krypton-ai/Krypton/main/a3.2/source.lua'))()
+  elseif _G.KryptonVersion == "a3.0" then
+    warn("Using version :" .. _G.KryptonVersion)
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Krypton-ai/Krypton/main/a3.0/source.lua'))()
+  elseif _G.KryptonVersion == "a2.5" then
+    warn("Using version :" .. _G.KryptonVersion)
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Krypton-ai/Krypton/main/a2.5/source.lua'))()
+  elseif _G.KryptonVersion == "a2.0" then
+    warn("Using version :" .. _G.KryptonVersion)
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Krypton-ai/Krypton/main/a2.0/Source.lua'))()
+  end
 end
 
--- Boot Krypton or rewind based on the given conditions
-if _G.License == true then
-    if _G.RewindToggle == true then
-        RewindKrypton()
-    else
-        KryptonBoot()
-    end
+function LicenseCheck()
+  if _G.License == true then
+    KryptonVersionCheck()
+  end
+end
+
+function KryptonVersionCheck()
+  if not _G.KryptonVersionCheck then
+    warn("Krypton is missing it's KryptonVersion variable add it back!")
+    print("defaulting to latest version")
+    loadstring(game:HttpGet(LatestVersionURL))()
+  else
+    KryptonBoot()
+  end
+end
+
+if not _G.License then
+  print("Using version : " .. _G.KryptonVersion)
+  warn("Krypton is missing it's license variable add it back!")
+  else
+    LicenseCheck()
 end
